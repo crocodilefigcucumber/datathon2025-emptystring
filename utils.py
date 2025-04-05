9976e3a8fe9d5d70ff48c326e6d437372101ad21
Elias <eliasmbarek@gmail.com>
2025-04-05 12:28:38 +0200

    Created utils file

---
+++ utils.py
@@ -0,0 +1,13 @@
+import tarfile
+import os
+
+def extract_all_tars(directory):
+    for filename in os.listdir(directory):
+        if filename.endswith((".tar", ".tar.gz", ".tgz")):
+            file_path = os.path.join(directory, filename)
+            try:
+                with tarfile.open(file_path, 'r:*') as tar:
+                    tar.extractall(path=directory)
+                    print(f"Extracted: {filename}")
+            except Exception as e:
+                print(f"Error extracting {filename}: {e}")
