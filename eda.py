0dbb4596b802167ca63427b6430423be5fb5bbc9
Michał Mikuta <mmikuta@ethz.ch>
2025-04-05 18:09:41 +0200

    git push bump

--- eda.py
+++ eda.py
@@ -8,16 +8,8 @@ import matplotlib.pyplot as plt
 
 from utilities.collect_csv import collect_to_csv
 
-if __name__=="__main__":
+if __name__ == "__main__":
     mode = "train"  # Default mode
-    if len(sys.argv) > 1:
-        if sys.argv[1].strip().lower() in ["--help", "-h"]:
-            print("Usage: python main.py [mode]")
-            print("mode: One of train, test, val, final")
-            sys.exit(0)
-        mode = sys.argv[1].strip().lower()
-    if mode not in ["train", "test", "val", "final"]:
-        raise ValueError("Invalid mode. Please enter one of: train, test, val, final.")
 
     if mode in ["train", "test", "val"]:
         # get list of clients
@@ -30,11 +22,11 @@ if __name__=="__main__":
         clients_dataframe = collect_to_csv(clients=clients, filename=dataset_csv)
     else:
         clients_dataframe = pd.read_csv(dataset_csv)
-    
+
     columns = clients_dataframe.columns
 
     gaps = np.array([sum(np.isna(clients_dataframe[col])) for col in columns])
-    incomplete = (gaps > 0)
+    incomplete = gaps > 0
 
     sns.set_theme()
     pl1 = sns.relplot(y=gaps[incomplete], x=columns[incomplete])
