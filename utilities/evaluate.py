aca27d4c23f51f1f99ac3f0b2df0ac8c765687fd
noahsta <noahst@student.ethz.ch>
2025-04-05 16:50:43 +0200

    evaluation with rules and examples

--- utilities/evaluate.py
+++ utilities/evaluate.py
@@ -2,16 +2,30 @@ import pandas as pd
 import os
 import numpy as np
 import json
+from typing import Tuple
 
-def evaluate(results: pd.DataFrame) -> int:
+def evaluate(results: pd.DataFrame) -> Tuple[pd.DataFrame, list, list]:
+    """
+    in: results DataFrame:              Accept comment
+                            client_0     Accept        
+                            client_1     Reject "Because he's poor"
+
+    out: pd.DataFrame: Confusion Matrix
+            list: False Negatives: Client should be rejected but NO rule has triggered
+            list: False Positives: Client should be accepted but was excluded by rule 
+            list: Rules giving False Positives: the corresponding rules triggered on good case
+
+    confusion_matrix, false_negatives, false_positives, false_positive_rules = evaluate(results)
+    """
     clients = results.index
 
     confus = pd.DataFrame(data=np.zeros((2,2)))
     confus.columns = ["predicted: Accept", "predicted: Reject"]
     confus.index = ["Accept", "Reject"]
 
-    rule_list = []
-    rule_index = pd.DataFrame(columns=["TP", "FP"])
+    false_positives = []
+    fp_rules = []
+    false_negatives = []
 
     for client in clients:
 
@@ -22,25 +36,21 @@ def evaluate(results: pd.DataFrame) -> int:
             file_data = json.load(file)
             label = file_data["label"]
         
-        """ # add rules to 
-        triggered_rules = client["comment"].split(",")
-        for rule in triggered_rules:
-            if not rule in rule_list:
-                rule_list.append(rule)
-                rule_index.append({"TP": 0, "FP": 0})
-                rule_index.index = rule_list """
 
-        # get confusion
+        # get confusion and record false assignments
         if label == "Accept":
             if results.loc[client, "Accept"] == label:
                 confus.loc["Accept", "predicted: Accept"] += 1
             else:
                 confus.loc["Accept", "predicted: Reject"] += 1
+                false_positives.append(client)
+                fp_rules.append(results.loc[client,"comment"].split(","))
         elif label == "Reject":
             if results.loc[client, "Accept"] == label:
                 confus.loc["Reject", "predicted: Reject"] += 1
             else:
                 confus.loc["Reject", "predicted: Accept"] += 1
+                false_negatives.append(client)
 
-    return confus
+    return confus, false_negatives, false_positives, fp_rules
         
